0d716036b4b641d46cc9204ee6d7e69aa9e9b7f1
Toph <christoph.holtmann@live.de>
2018-06-15 16:26:21 +0200

    Add hand tracking and destroy functionality

---
+++ Assets/Scripts/Interaction/Destroyable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject onDestroyObject = null;
+
+    public void Destroy()
+    {
+        if(onDestroyObject != null)
+        {
+            var destroyObject = Instantiate(onDestroyObject);
+
+            destroyObject.transform.position = transform.position;
+            destroyObject.transform.rotation = transform.rotation;
+        }
+
+        Destroy(gameObject);
+    }
+}
