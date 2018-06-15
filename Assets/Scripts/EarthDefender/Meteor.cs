cd0afb2a66378fb1a469366313215bcd0782c5b3
Sergej <sergej1995@gmail.com>
2018-06-15 20:09:36 +0200

    Fix previous merge

--- Assets/Scripts/EarthDefender/Meteor.cs
+++ Assets/Scripts/EarthDefender/Meteor.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Meteor : MonoBehaviour {
 
     private bool moving = false;
-    private const float _speed = 1f;
+    private const float _speed = 0.75f;
+   
 
 	void moveMeteor()
     {
+       
         Vector3 posEarth = Backend.getEarthPos();
         Vector3 direction = posEarth - transform.position;
+        direction.Normalize();
         transform.position = transform.position + (Time.deltaTime * _speed * direction);
     }
 	
